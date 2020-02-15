0693fed098574ce961b94c7bb0d23d64c9107bee
Mike Moran <mike@houseofmoran.com>
2020-02-15 14:19:56 +0000

    assign all days in year to a closest match

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ fn main() {
     let offset_names = ["quarter past", "half past"];
     let num_months: u32 = 12;
 
+    let mut assignments = Vec::new();
+
     for year_offset in 0..num_years {
         let year = start_year + year_offset;
         let year_end = Utc.ymd(year + 1, 1, 1) - Duration::days(1);
@@ -26,18 +28,38 @@ fn main() {
                     let days_offset =
                         (days_till_year_end.num_days() as f32 * offset).floor() as i64;
                     let date = last_day_of_month + Duration::days(days_offset);
+                    let relative_date = format!("{} {}", offset_name, month_name);
                     println!(
-                        "{} + ({} * {} = {}) = {}, {} {}",
+                        "{} + ({} * {} = {}) = {}, {}",
                         last_day_of_month,
                         days_till_year_end,
                         offset,
                         days_offset,
                         date,
-                        offset_name,
-                        month_name
+                        relative_date
                     );
+                    assignments.push((date, relative_date));
                 }
             }
         }
+    }
+
+    assignments.sort_by_key(|e| e.0);
+    let start_date = Utc.ymd(start_year, 1, 1);
+    let max_date = Utc.ymd(start_year + num_years, 1, 1) - Duration::days(1);
+    let mut current_date = start_date;
+    while current_date <= max_date {
+        let closest = match assignments.binary_search_by_key(&current_date, |e| e.0) {
+            Ok(index) => assignments[index].clone(),
+            Err(index) => {
+                if index < assignments.len() {
+                    assignments[index].clone()
+                } else {
+                    assignments[assignments.len() - 1].clone()
+                }
+            }
+        };
+        println!("{} -> {:?}", current_date, closest);
+        current_date = current_date + Duration::days(1);
     }
 }
